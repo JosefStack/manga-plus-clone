e3a5b9f3a51ff9ca7e5ccc26665194aa80751b61
JosefStack <jojohnson350@gmail.com>
2026-01-14 16:28:01 +0200

    odd logic

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,16 +1,14 @@
+import { useDeferredValue } from "react";
+
 const carousel = document.querySelector(".carousel");
 const carouselItems = document.querySelectorAll(".carousel-item");
 
 let visible = 2;
 
 const getVisibleCount = function() {
-    const isMobile = window.matchMedia("(max-width: 768px").matches;
+    const isMobile = window.matchMedia("max-width: 768px").matches;
 
-    if (isMobile) {
-        visible = 1;
-    } else {
-        visible = 2;
-    }
+    visible = isMobile ? 1 : 2;
 }
 
 getVisibleCount();
@@ -19,16 +17,29 @@ window.addEventListener("resize", getVisibleCount);
 let index = 0;
 const max = carouselItems.length;
 
+let odd = false;
+
+if (max % 2 != 0) {
+    odd = true;
+}
+
 const slider = function() {
     let gap = parseFloat(getComputedStyle(carousel).gap);
 
     let width = carouselItems[0].clientWidth;
     carousel.style.transform = `translateX(-${index * width + index * gap}px)`;
-
+    
     index += visible;
     if (index >= max) {
         index = 0;
     }
+
+    if (odd && (visible === 2)) {
+        if (index === max - 1) {
+            index--;
+        }
+    }
+
 }
 
 
