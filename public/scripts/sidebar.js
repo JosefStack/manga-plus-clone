4a052e267b3e0c16e6058e2e6643480431574fa1
JosefStack <jojohnson350@gmail.com>
2026-01-16 14:06:29 +0200

    added sidebar, mobile responsive

---
+++ public/scripts/sidebar.js
@@ -0,0 +1,14 @@
+const toggleButton = document.querySelector(".toggleButtonContainer");
+const header = document.getElementById("header-container");
+const sidebar = document.getElementById("sidebar-container");
+const body = document.body;
+
+toggleButton.addEventListener("click", function() {
+
+    header.classList.toggle("activeSidebar");
+    toggleButton.classList.toggle("open");
+    sidebar.classList.toggle("open");
+    body.classList.toggle("activeSidebar");
+
+
+})
